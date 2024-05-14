e46ca691ca4112b6d1c3bb5c9e6fa5ac6a222ecc
corbyn-jpg <159898463+corbyn-jpg@users.noreply.github.com>
2024-05-14 20:27:16 +0200

    Plus, minus code

---
+++ js/scripts.js
@@ -0,0 +1,32 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const numberContainers = document.querySelectorAll('.number');
+    
+    numberContainers.forEach(container => {
+        const numberInput = container.querySelector('input[type="text"]');
+        const minusBtn = container.querySelector('.btn.minus');
+        const plusBtn = container.querySelector('.btn.plus');
+        
+        minusBtn.addEventListener("click", function() {
+            let currentValue = parseInt(numberInput.value);
+            if (currentValue > 0) {
+                numberInput.value = currentValue - 1;
+                plusBtn.removeAttribute("disabled");
+            }
+            if (currentValue === 1) {
+                this.setAttribute("disabled", "disabled");
+            }
+        });
+        
+        plusBtn.addEventListener("click", function() {
+            let currentValue = parseInt(numberInput.value);
+            if (currentValue < parseInt(numberInput.max)) {
+                numberInput.value = currentValue + 1;
+                minusBtn.removeAttribute("disabled");
+            }
+            if (currentValue === parseInt(numberInput.max) - 1) {
+                this.setAttribute("disabled", "disabled");
+            }
+        });
+    });
+});
+
