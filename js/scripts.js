864a2df30680f442b1e34644a857aabb899eebf6
corbyn-jpg <159898463+corbyn-jpg@users.noreply.github.com>
2024-05-20 12:53:22 +0200

    Adding filter bar functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,100 @@
+//Functionalise the filter bar
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("resultsPerPage").addEventListener("change", function() {
+        var selectedValue = parseInt(this.value);
+        console.log("Results Per Page:", selectedValue);
+        updateResultsPerPage(selectedValue);
+    });
+
+    document.getElementById("sortBy").addEventListener("change", function() {
+        var selectedValue = this.value;
+        console.log("Sort By:", selectedValue);
+        updateSortLayout(selectedValue);
+    });
+
+    function updateResultsPerPage(numPerPage) {
+        var planets = document.querySelectorAll(".available > .planetPurple, .available > .planetBlue");
+        planets.forEach(function(planet, index) {
+            if (index < numPerPage) {
+                planet.style.display = "";
+            } else {
+                planet.style.display = "none";
+            }
+        });
+    }
+
+var originalOrder = Array.from(document.querySelectorAll('.planetPurple, .planetBlue'));
+
+function resetLayout() {
+    var availableSection = document.querySelector('.available');
+    availableSection.innerHTML = ''; // Clear the existing content
+    originalOrder.forEach(function(planet) {
+        availableSection.appendChild(planet);
+    });
+}
+
+var originalOrder = Array.from(document.querySelectorAll('.planetPurple, .planetBlue'));
+
+function resetLayout() {
+    var availableSection = document.querySelector('.available');
+    availableSection.innerHTML = '';
+    originalOrder.forEach(function(planet) {
+        availableSection.appendChild(planet);
+    });
+}
+
+function updateSortLayout(sortOption) {
+    var sortedPlanets;
+    if (sortOption === 'default') {
+        resetLayout();
+        return;
+    }
+    var planets = document.querySelectorAll('.planetPurple, .planetBlue');
+    planets = Array.from(planets);
+    switch (sortOption) {
+        case 'priceLowToHigh':
+            sortedPlanets = planets.sort(function(a, b) {
+                var priceA = parseFloat(a.querySelector('.availableText h3').innerText.replace(/[^\d.-]/g, ''));
+                var priceB = parseFloat(b.querySelector('.availableText h3').innerText.replace(/[^\d.-]/g, ''));
+                return priceA - priceB;
+            });
+            break;
+        case 'priceHighToLow':
+            sortedPlanets = planets.sort(function(a, b) {
+                var priceA = parseFloat(a.querySelector('.availableText h3').innerText.replace(/[^\d.-]/g, ''));
+                var priceB = parseFloat(b.querySelector('.availableText h3').innerText.replace(/[^\d.-]/g, ''));
+                return priceB - priceA;
+            });
+            break;
+        case 'dateNewestFirst':
+            sortedPlanets = planets.sort(function(a, b) {
+                var dateA = new Date(parseDate(a.querySelector('.availableText p:last-of-type').innerText));
+                var dateB = new Date(parseDate(b.querySelector('.availableText p:last-of-type').innerText));
+                return dateA - dateB;
+            });
+            break;
+        case 'dateOldestFirst':
+            sortedPlanets = planets.sort(function(a, b) {
+                var dateA = new Date(parseDate(b.querySelector('.availableText p:last-of-type').innerText));
+                var dateB = new Date(parseDate(a.querySelector('.availableText p:last-of-type').innerText));
+                return dateA - dateB;
+            });
+            break;
+        default:
+            sortedPlanets = planets;
+    }
+    var availableSection = document.querySelector('.available');
+    availableSection.innerHTML = '';
+    sortedPlanets.forEach(function(planet) {
+        availableSection.appendChild(planet);
+    });
+}
+
+function parseDate(dateString) {
+    var dates = dateString.split(' - ');
+    return dates[1];
+}
+});
 
 
 //Plus/minus button code
